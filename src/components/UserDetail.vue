47f8722ed239881f94f205a96b720f078e95fd55
Cuevas, Víctor <victor.cuevas@adesso.es>
2020-01-30 16:06:53 +0100

    - Added UserDetail component

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-col
+            cols="5"
+            md="5"
+    >
+        <h2>Selected user info:</h2>
+        <v-card class="mx-auto" max-width="300" loading="true">
+            <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    v-bind:src="item.picture"
+            >
+            </v-img>
+            <v-card-subtitle class="pb-0">{{ item.lastName }} {{ item.firstName }}</v-card-subtitle>
+            <v-card-text class="text--primary">
+                <div>{{ item.address }}</div>
+                <div>{{ item.email }}</div>
+                <div>{{ item.gender }}</div>
+            </v-card-text>
+        </v-card>
+    </v-col>
+</template>
+
+<script>
+    export default {
+        name: 'UserList',
+        props: {
+            item: Object
+        },
+    }
+</script>
+
+<style scoped>
+  .v-card {
+    margin: 35px auto
+  }
+</style>
--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,19 +1,63 @@
 <template>
-  <div>
-    <ul class="col-md-6">
-      <li v-for="(item, index) in data" :key="index">
-        <span>{{item.lastName}} {{item.firstName}}</span>
-      </li>
-    </ul>
-    <user-detail></user-detail>
-  </div>
+    <v-container class="grey lighten-5">
+      <v-row>
+        <v-col
+                  cols="7"
+                  md="7"
+          >
+          <h2>User list:</h2>
+          <v-simple-table>
+                <template v-slot:default>
+                    <thead>
+                    <tr>
+                        <th class="text-left">Last name</th>
+                        <th class="text-left">First name</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="(item, index) in data" :key="index" v-on:click="getUserById(item.userId)">
+                        <td>{{ item.lastName }}</td>
+                        <td>{{ item.firstName }}</td>
+                    </tr>
+                    </tbody>
+                </template>
+            </v-simple-table>
+          </v-col>
+          <user-detail v-bind:item="selectedItem" v-if="selectedItem"></user-detail>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-export default {
-  name: 'UserList',
-  props: {
-    data: Array
-  },
-}
+    import UserDetail from "./UserDetail";
+
+    export default {
+        name: 'UserList',
+        props: {
+            data: Array
+        },
+        data: function () {
+            return {
+                selectedItem: this.selectedItem
+            }
+        },
+        methods: {
+            getUserById(userId) {
+                const axios = require('axios');
+                if (!this.selectedItem || userId !== +this.selectedItem.userId) {
+                    axios.get(`${this.endpoint}/user/${userId}`).then(response => (this.selectedItem = response.data));
+                }
+            }
+        },
+        components: {
+            UserDetail
+        }
+    }
 </script>
+
+<style scoped>
+  .v-data-table {
+    margin: 15px auto
+  }
+</style>
+
